d7e4bef8b292716a58e5d7e9d5a821a58910e981
guy nut <82942709+guynut@users.noreply.github.com>
2023-12-06 19:01:46 +0700

    📌 myRouter.js

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -6,6 +6,8 @@ const Product = require('../models/products')
 
 // UploadFile
 const multer = require('multer')
+const User = require('../models/User')
+const { isNamedExportBindings, NewLineKind } = require('typescript')
 // Create Storage
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
@@ -20,42 +22,65 @@ const upload = multer({
     storage:storage
 })
 
+// Dynamic file (EJS) use render "Index file"
+router.get('/',(req,res)=>{
+    Product.find().exec((err,doc)=>{
+        res.render('index',{products:doc, username: req.session.username})
+    })
+})
+
+// Add User
+router.get('/createuser',(req,res)=>{
+    res.render('createuser',{username: req.session.username})
+})
+router.post('/addUser',(req,res) =>{
+    let data = new User({
+        username:req.body.username,
+        password:req.body.password
+    })
+    User.addUser(data,(err)=>{
+        if(err) console.log(err)
+    })
+    res.redirect('/')
+});
+
 // Cookie check Login
 router.post('/login',(req,res)=>{
     const username = req.body.username
     const password = req.body.password
     const timeExpire = 1000000 // 1000Second
 
-    if (username === "admin" && password === "123"){
-        req.session.username = username
-        req.session.password = password
-        req.session.login = true
-        req.session.cookie.maxAge = timeExpire
-        res.redirect('/manage')
-    }else {
-        res.render('404')
+    User.findOne({username:username,password:password}).exec((err,data)=>{
+        if (data === null ){
+            res.render('admin',{username: req.session.username})
+        } else{
+            req.session.username = username
+            req.session.password = password
+            req.session.login = true
+            req.session.cookie.maxAge = timeExpire
+            res.redirect('/')
+        }
+    })
+    if (req.session.login === false){
+        res.render('404',{username: req.session.username})
     }
 })
 // Cookie check Logout
 router.get('/logout',(req,res)=>{
     req.session.destroy((err)=>{
-        res.redirect('/manage')
+        res.redirect('/')
     })
 })
 
-// Dynamic file (EJS) use render
-router.get('/',(req,res)=>{
-    Product.find().exec((err,doc)=>{
-        res.render('index',{products:doc})
-    })
+router.get('/loginBtn',(req,res) => {
+    res.render('admin',{username: req.session.username})
 })
-
 // Rout add-product page check with Cookie
 router.get('/add-product',(req,res)=>{
     if(req.session.login){
-        res.render('form')
+        res.render('form',{username: req.session.username})
     }else{
-        res.render('admin')
+        res.render('admin',{username: req.session.username})
     }
 })
 
@@ -63,10 +88,10 @@ router.get('/add-product',(req,res)=>{
 router.get('/manage',(req,res)=>{
     if(req.session.login){
         Product.find().exec((err,doc)=>{
-            res.render('manage',{products:doc})
+            res.render('manage',{products:doc, username: req.session.username})
         })
     }else{
-        res.render('admin')
+        res.render('admin',{username: req.session.username})
     }
 })
 
@@ -97,15 +122,16 @@ router.post('/insert',upload.single("image"),(req,res)=>{
 router.get('/:id',(req,res)=> {
     const product_id = req.params.id
     Product.findOne({_id:product_id}).exec((err,doc)=>{
-        res.render('product',{product:doc})
+        res.render('product',{product:doc,username: req.session.username})
     })
 })
 
+// Go to Edit data check with ID
 router.post('/edit',(req,res)=>{
     const edit_id = req.body.edit_id
     Product.findOne({_id:edit_id}).exec((err,doc)=>{
         // show data to form
-        res.render('edit',{product:doc})
+        res.render('edit',{product:doc,username: req.session.username})
     })
 })
 router.post('/update',(req,res)=>{
